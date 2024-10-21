7595763824521f1ef2d6a160988e2c013da722b7
ThePacktur <paulo.fernandez91@gmail.com>
2024-10-21 14:35:39 -0300

    update delete
    update delete in views

--- projectDjango/projectDjangoApp/admin.py
+++ projectDjango/projectDjangoApp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from projectDjangoApp.models import Proyecto
+
+class ProyectoAdmin(admin.ModelAdmin):
+    list_display = ['fechaInicio', 'fechaTermino', 'nombre', 'responsable', 'prioridad']
 
 # Register your models here.
+admin.site.register(Proyecto)
